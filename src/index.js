b267c24f1c5118d47da016208891fdd49af48447
Junior Dugue <jrdugue@gmail.com>
2019-06-17 18:16:02 -0400

    learned about import Link from react router dom, added Link to my navs
    learned about import Link from react router dom, added Link to my nav links

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ add react router dependncy using yarn add react-router-dom and youll wrap the
 <App/> component within the <Router> component
 */
 }
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 {
   /*
 remember after creating a CRA.. to cd into the app folder, and then 
@@ -28,8 +28,14 @@ class App extends React.Component {
       // you can also wrap your main div App in router component as well
       //         as an alternative to wrapping the <App/>
       <div className="App">
-        <h1>Hello CodeSandbox</h1>
-        <h2>Start editing to see some magic happen!</h2>
+        <nav>
+          <h1>Juniors</h1>
+          <div className="nav-links">
+          <Link to="/">Home</Link>
+          <br></br>
+          <Link to="/item-list">ItemList</Link>
+          </div>
+        </nav>
         <Route path="/" component={Home} />{" "}
         {/* when using Route, import Route as well e.g. import { BrowserRouter as Router, Route } from "react-router-dom"; */}
         <Route path="/item-list" component={ItemList} />
