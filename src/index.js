9dee25ac331a0216224bd6434afd445797baff2f
Junior Dugue <jrdugue@gmail.com>
2019-06-17 14:48:10 -0400

    refactored index to a class component
    refactored index functional component into a class component

--- src/index.js
+++ src/index.js
@@ -1,18 +1,37 @@
 import React from "react";
 import ReactDOM from "react-dom";
-// add react router dependncy using yarn add react-router-dom and youll wrap the <App/> component within the <Router> component
+{
+  /*  
+add react router dependncy using yarn add react-router-dom and youll wrap the 
+<App/> component within the <Router> component
+*/
+}
 import { BrowserRouter as Router } from "react-router-dom";
-//remember to after creating a CRA.. to cd into the app folder, and then run npm or yarn for react-router-dom
-
+{
+  /*
+remember after creating a CRA.. to cd into the app folder, and then 
+run npm or yarn for react-router-dom, then npm / yarn start
+*/
+}
 import "./styles.css";
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+  }
+
+  render() {
+    return (
+      <Router>
+        {" "}
+        {/* you can also wrap your main div App in router component as well as an alternative to wrapping the <App/> */}
+        <div className="App">
+          <h1>Hello CodeSandbox</h1>
+          <h2>Start editing to see some magic happen!</h2>
+        </div>
+      </Router>
+    );
+  }
 }
 
 const rootElement = document.getElementById("root");
